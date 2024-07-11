eddaa718d23db50136613394f9b708eecb1c7778
RootUser <demo.helloworld.1@gmail.com>
2024-07-11 08:37:58 +0000

    2nd Commit for Connect

--- Programs/NodeJS Events/Connect.js
+++ Programs/NodeJS Events/Connect.js
@@ -3,18 +3,30 @@ var http = require('http');
 
 var app = connect();
 
-function doFirst(request,response, next){
-    console.log("Bacon");
-    next();
+// function doFirst(request,response, next){
+//     console.log("Bacon");
+//     next();
+// }
+
+// function doSecond(request,response, next){
+//     console.log("Tuna");
+//     next();
+// }
+
+// app.use(doFirst);
+// app.use(doSecond);
+
+function profile(request,response){
+    console.log("User requested for profile");
 }
 
-function doSecond(request,response, next){
-    console.log("Tuna");
-    next();
+function form(request,response){
+    console.log("User requested for Form");
 }
 
-app.use(doFirst);
-app.use(doSecond);
+
+app.use('/profile',profile);
+app.use('/form',form);
 
 http.createServer(app).listen(8080);
 
