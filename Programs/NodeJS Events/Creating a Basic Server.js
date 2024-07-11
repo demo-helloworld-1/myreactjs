259c37354e84b61e0dee147be1722f8dbb6dbd2a
RootUser <demo.helloworld.1@gmail.com>
2024-07-11 08:06:34 +0000

    Web File Server

--- Programs/NodeJS Events/Creating a Basic Server.js
+++ Programs/NodeJS Events/Creating a Basic Server.js
@@ -2,10 +2,14 @@ var http = require("http");
 
 function onRequest(request, response){
     console.log("A user made an request " + request.url);
-    response.writeHead(200,{"Cone"})
+    response.writeHead(200,{"Context-Type": "text/plain"});
+    response.write("Here is your Data");
+    response.end();
 }
 
 
 http.createServer(onRequest).listen(8080);
 
-console.log("Helloworld, The server is now Running");
+console.log("The server is now Running...");
+
+//When we run this code, there will be a server enabled and active 
